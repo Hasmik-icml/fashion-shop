1da940d89bb0e1f9091a48da547c9310ea737ba3
Hasmik-icml <gevorgyan.icml@gmail.com>
2022-04-05 19:25:06 +0400

    adding Tabs component

--- src/Components/Tabs/Tabs.jsx
+++ src/Components/Tabs/Tabs.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import { Tab } from 'semantic-ui-react'
+import DataTable from '../dataTable/DataTable'
 
-const panes = [
-  { menuItem: 'Tab 1', render: () => <Tab.Pane>Tab 1 Content</Tab.Pane> },
-  { menuItem: 'Tab 2', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
-  { menuItem: 'Tab 3', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
-]
-
-const TabExampleVerticalTabularRight = () => (
-  <Tab menu={{ fluid: true, vertical: true, tabular: 'right' }} panes={panes} />
-)
-
-export default TabExampleVerticalTabularRight
+function Tabs(pendingProducts, allProducts){
+    const panes = [
+        {
+             menuItem: 'ALL PRODUCTS', render: () => <Tab.Pane>Tab 1 Content</Tab.Pane> },
+        {
+             menuItem: 'PENDING', render: () => {
+              <Tab.Pane>
+                  <DataTable />
+              </Tab.Pane> 
+             }
+        },
+        
+      ];
+return (
+    <Tab menu={{ fluid: true, vertical: true, tabular: 'right' }} panes={panes} />
+);
+};
+export default Tabs;
