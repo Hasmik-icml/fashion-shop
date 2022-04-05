502d9fcf52444db752b732878195cfbac2171a4d
Hasmik-icml <gevorgyan.icml@gmail.com>
2022-04-05 19:18:26 +0400

    adding dataTable component

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -13,82 +13,54 @@ function Dashboard() {
   const { error, isAuthenticated, isLoading, user, getAccessTokenSilently } =
     useAuth0();
   const [orderList, setOrderList] = useState([]);
+  const [pendingProducts, setpendingProducts] = useState([]);
+  const [allProducts, setAllProducts] = useState([]);
 
   async function orderShow() {
     try {
       const token = await getAccessTokenSilently();
 
       let data = null;
-      if (user[`${domainName}roles`] == USER){
-        data = await getOrderByStatus(user.sub, token, UNPAID)
+      if (user && user[`${domainName}roles`] == ADMIN){
+        data = await getOrderByStatus(user.sub, token, UNPAID);
+        setpendingProducts(data);
       }else {
         data = await getOrders(user.sub, token, UNPAID);
+        if (data && Array.isArray(data)) {
+          if (data.length !== 0) setOrderList(data);
+        } else if (data && data.status === 401) {
+          const authorised = await authoriseUser(user, token);
+        } else {
+          console.log("hajox");
+        }
+      } 
+    }catch (error) {
+        console.log("hajox chi");
       }
-     
-      console.log(data);
-      // data = await getAllOrders(user.sub, token, PENDING)
- 
-
-      if (data && Array.isArray(data)) {
-        if (data.length !== 0) setOrderList(data);
-      } else if (data && data.status === 401) {
-        const authorised = await authoriseUser(user, token);
-      } else {
-        console.log("hajox");
-      }
-    } catch (error) {
-      console.log("hajox chi");
     }
-  }
+
+
   useEffect(() => {
       console.log("use effect call");
       if(user) orderShow();
-   
+    console.log("user==",user);
+    if (user) {
+      console.log("userDomain", user[`${domainName}roles`]);
+    }
   }, [user]);
+
   return (
     <div className="dashboard ui container">
-      it's dashboard
       {user &&
       user[`${domainName}roles`] &&
       user[`${domainName}roles`].includes("admin") ? (
         <>
-          <button>Add New Product</button>
-          <Table celled>
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell>Name</Table.HeaderCell>
-                <Table.HeaderCell>Status</Table.HeaderCell>
-                <Table.HeaderCell>Notes</Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              <Table.Row>
-                <Table.Cell>Product1</Table.Cell>
-                <Table.Cell>Pending</Table.Cell>
-                <Table.Cell negative>Notes</Table.Cell>
-              </Table.Row>
-              <Table.Row positive>
-                <Table.Cell>Product2</Table.Cell>
-                <Table.Cell>
-                  <Icon name="checkmark" />
-                  Pending
-                </Table.Cell>
-                <Table.Cell>Notes</Table.Cell>
-              </Table.Row>
-              <Table.Row>
-                <Table.Cell>Product3</Table.Cell>
-                <Table.Cell>Unknown</Table.Cell>
-                <Table.Cell positive>
-                  <Icon name="close" />
-                  Remove
-                </Table.Cell>
-              </Table.Row>
-            </Table.Body>
-          </Table>
+          <AddProduct/>
+        {/* <Tabs/> */}
         </>
       ) : (
         <>
-      <AddProduct/>
+      {/* <DataTable/> */}
       </>
       )}
     </div>
