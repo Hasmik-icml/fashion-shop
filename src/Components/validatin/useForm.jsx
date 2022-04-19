0b2ba2807bc30b170f91dda0760f58591b0740db
Hasmik-icml <gevorgyan.icml@gmail.com>
2022-04-19 21:24:16 +0400

    formating updating
    formating updating

---
+++ src/Components/validatin/useForm.jsx
@@ -0,0 +1,23 @@
+
+const useForm = (callback) => {
+
+    const [value, setValue] = useState(initialValue);
+
+    const [errors, setErrors] = useState({});
+  
+
+  function handleChange(event){
+    validate(event,name,val);
+
+    changeOptions({[event.target.name]:event.target.value});
+  }
+  
+     return {
+        values,
+        errors,
+        handleChange,
+        handleSubmit,
+        changeOptions
+    }
+  }
+  
