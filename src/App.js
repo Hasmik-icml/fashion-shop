a5b92b51046af0313b1b442d103b8979289adc50
Hasmik-icml <gevorgyan.icml@gmail.com>
2022-04-19 21:09:35 +0400

    Loading
    creating loading component

--- src/App.js
+++ src/App.js
@@ -7,20 +7,25 @@ import Products from "./Components/products/products.jsx";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import LoginPage from "./Components/Login/Loginbutton.jsx";
 import { Parent } from "./Components/UseContext.jsx";
+import { useAuth0 } from '@auth0/auth0-react';
+import Loading from "./Components/Loading/Loading.jsx";
 
 function App() {
+  const {isLoading} = useAuth0();
   return (
     <BrowserRouter>
+   {isLoading ? <Loading/> :
       <div className="App">
-        <Header className="ui fixed inverted main menu" />
-        <Routes>
+          <Header className="ui fixed inverted main menu" />
+          <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/products" element={<Products />}></Route>
           <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/products" element={<Products />}></Route>
           <Route path="/login" element={<LoginPage />}></Route>
         </Routes>
         {/* <Footer /> */}
       </div>
+}
     </BrowserRouter>
   );
 }
